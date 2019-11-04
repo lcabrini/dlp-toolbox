c662e72475d0075f43b01d8e242c0094e6deb0aa
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-04 17:43:59 +0000

    Send SMS on failed ping.

--- pinger/pinger.py
+++ pinger/pinger.py
@@ -2,18 +2,48 @@
 
 import time
 import threading
+import requests
 import schedule
 import pingparsing
 import config
 
+def sms(msg):
+    url = "http://{}/bulksms/bulksms".format(config.SMS['url'])
+    destination = ','.join(config.SMS['recipients'])
+    #for recipient in config.SMS['recipients']:
+    #print("recp: {}".format(recipient))
+    params = {
+            "username": config.SMS['username'],
+            "password": config.SMS['password'],
+            "type": "0",
+            "dlr": "1",
+            "destination": destination,
+            "source": "Delaphone",
+            "message": str(msg),
+            }
+    resp = requests.get(url, params=params)
+    print("SMS: to {}, resp: {}".format(destination, resp))
+
 def ping(host):
+    #print(host)
     parser = pingparsing.PingParsing()
     transmitter = pingparsing.PingTransmitter()
     transmitter.destination = host
     transmitter.count = 5
     result = transmitter.ping()
-    stats = parser.parse(result).as_dict()
-    print(stats['rtt_avg'])
+    if result.returncode != 0:
+        message = """This is a test message. Ping {}, down.
+        """.format(host)
+        sms(message)
+    else:
+        #print(result)
+        parser.parse(result)
+        stats = parser.as_dict()
+        #print(stats['packet_loss_count'])
+        if stats['packet_loss_count'] > 0:
+            message = """This is a test message. Ping {}, packet loss.
+            """.format(stats['destination'])
+            sms(message)
 
 def ping_job():
     threads = list()
