ae3b6accf5872a7097d19f8fcf278e83775729a8
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-04 15:09:51 +0000

    Skeleton pinger program.

--- pinger/pinger.py
+++ pinger/pinger.py
@@ -1,1 +1,21 @@
 #! /usr/bin/env python3
+
+import shlex
+import subprocess
+import time
+import schedule
+import config
+
+def ping():
+    for ip in config.HOSTS:
+        cmd = "ping -c 5 {}".format(ip)
+        args = shlex.split(cmd)
+        proc = subprocess.run(args, capture_output=True)
+        for line in proc.stdout.splitlines():
+            print("LINE: {}".format(line))
+
+
+schedule.every(10).seconds.do(ping)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
