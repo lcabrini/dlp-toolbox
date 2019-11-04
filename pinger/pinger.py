e1d22c4b8ee64481c8be82962909901d3599f67c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-04 16:09:26 +0000

    Clean up.

--- pinger/pinger.py
+++ pinger/pinger.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python3
 
-#import shlex
-#import subprocess
 import time
 import threading
 import schedule
@@ -16,15 +14,6 @@ def ping(host):
     result = transmitter.ping()
     stats = parser.parse(result).as_dict()
     print(stats['rtt_avg'])
-    #print(json.dumps(parser.parse(result).as_dict(), indent=2))
-    #cmd = "ping -c 5 {}".format(ip)
-    #args = shlex.split(cmd)
-    #proc = subprocess.run(args, capture_output=True)
-    #for line in proc.stdout.splitlines()[-2:]:
-    #    s = line.decode("UTF-8")
-    #    print(s)
-    #    #pass
-    #    #print("LINE: {}".format(line.decode("UTF-8")))
 
 def ping_job():
     threads = list()
@@ -32,7 +21,6 @@ def ping_job():
         t = threading.Thread(target=ping, args=(host,), daemon=True)
         threads.append(t)
         t.start()
-    #print("started {} threads".format(len(threads)))
 
 schedule.every(10).seconds.do(ping_job)
 while True:
