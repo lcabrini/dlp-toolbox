b82459a5f30d49c653c76db0555647e370531ec7
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-14 17:08:40 +0000

    Implemented check_sudo_password function.

--- python/delaphone/toolbox/user.py
+++ python/delaphone/toolbox/user.py
@@ -1,5 +1,6 @@
 import subprocess
 import shlex
+import pexpect
 
 def can_sudo():
     cmd = shlex.split("sudo -nv")
@@ -8,3 +9,16 @@ def can_sudo():
         return True
     except subprocess.CalledProcessError as e:
         return e.output.decode().startswith("sudo:")
+
+def check_sudo_password(password):
+    child = pexpect.spawn("sudo echo 1")
+    child.expect("[sudo]*: ")
+    child.sendline(password.encode())
+    i = child.expect(['[sudo]*:', '1'])
+    if i == 0:
+        return False
+    elif i == 1:
+        return True
+    else:
+        # Hopefully we never get here
+        return False
