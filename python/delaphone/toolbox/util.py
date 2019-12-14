ac835edbbe005660f01044085203f0277c0aaff8
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-14 14:51:53 +0000

    Colorize ask_yesno function.

--- python/delaphone/toolbox/util.py
+++ python/delaphone/toolbox/util.py
@@ -1,10 +1,12 @@
+from colorama import Fore, Style
+
 def ask_yesno(prompt):
     while True:
-        ans = input("{} (yes/no)? ".format(prompt)).strip().lower()
+        ans = input("{}{} (yes/no)?{} ".format(
+            Style.BRIGHT, prompt, Style.RESET_ALL)).strip().lower()
         if ans in ('n', 'no'):
             return False
         elif ans in ('y', 'yes'):
             return True
         else:
             print("Unrecognized answer. Try again")
-
