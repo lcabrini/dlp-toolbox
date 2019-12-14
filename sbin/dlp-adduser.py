38f6e56c8d84e181704afc390ad9417b893a21fa
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-14 14:20:36 +0000

    Add some color to the output.

--- sbin/dlp-adduser.py
+++ sbin/dlp-adduser.py
@@ -1,8 +1,11 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 
 import sys
 import os
 from getpass import getpass
+
+import colorama
+from colorama import Fore, Style
 
 # TODO: add proper path
 sys.path.append("python")
@@ -12,14 +15,17 @@ if os.geteuid() == 0:
     print("This program should not be run as root")
     sys.exit(1)
 
+colorama.init()
+
 sudo_intro = """
 You will be required to give your password for sudo access. This will
-be used both on the local and remote hosts. It is therefore assumed that
-your password is the same on all the affected hosts and that you have
-sudo rights on all the hosts.
+be used both on the local and remote hosts.
+
+{}It is therefore assumed that your password is the same on all the
+affected hosts and that you have sudo rights on all the hosts.{}
 
 If this is not the case you may want to exit now.
-"""
+""".format(Fore.YELLOW, Style.RESET_ALL)
 
 print(sudo_intro.strip())
 if not ask_yesno("Do you want to continue"):
