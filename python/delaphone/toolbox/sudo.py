25a6538bc30f56631499ef988000679ff3117de6
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-15 11:24:50 +0000

    Moved sudo-related functions to sudo.py.

---
+++ python/delaphone/toolbox/sudo.py
@@ -0,0 +1,24 @@
+import shlex
+import subprocess
+import pexpect
+
+def can_sudo():
+    cmd = shlex.split("sudo -nv")
+    try:
+        out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        return True
+    except subprocess.CalledProcessError as e:
+        return e.output.decode().startswith("sudo:")
+
+def check_sudo_password(password):
+    child = pexpect.spawn("sudo echo 1")
+    child.expect("[sudo]*: ")
+    child.sendline(password.encode())
+    i = child.expect(['[sudo]*:', '1'])
+    if i == 0:
+        return False
+    elif i == 1:
+        return True
+    else:
+        # Hopefully we never get here
+        return False
