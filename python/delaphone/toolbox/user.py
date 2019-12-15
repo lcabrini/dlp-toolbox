35fc28013754f7e477c05d88ccb9509d2eb9ca03
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-15 13:46:15 +0000

    Implemented user_add().

--- python/delaphone/toolbox/user.py
+++ python/delaphone/toolbox/user.py
@@ -3,27 +3,10 @@ import random
 import subprocess
 import shlex
 import pexpect
+from delaphone.toolbox.sudo import *
 
-def can_sudo():
-    cmd = shlex.split("sudo -nv")
-    try:
-        out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-        return True
-    except subprocess.CalledProcessError as e:
-        return e.output.decode().startswith("sudo:")
-
-def check_sudo_password(password):
-    child = pexpect.spawn("sudo echo 1")
-    child.expect("[sudo]*: ")
-    child.sendline(password.encode())
-    i = child.expect(['[sudo]*:', '1'])
-    if i == 0:
-        return False
-    elif i == 1:
-        return True
-    else:
-        # Hopefully we never get here
-        return False
+class UserExists(Exception):
+    pass
 
 def generate_password():
     # This may change later.
@@ -38,3 +21,14 @@ def user_exists(username):
             if line.startswith("{}:".format(username)):
                 return True
     return False
+
+def add_user(sudo_password, username, password):
+    if user_exists(username):
+        raise UserExists()
+
+    child = interactive_sudo(sudo_password)
+    child.sendline("useradd -m {}".format(username))
+    child.expect("#")
+    child.sendline("chpasswd <<< '{}:{}'".format(username, password))
+    child.expect("#")
+    child.close()
