7d8f91c9c31325fa0f17c5856e8d95f5a58006f9
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-17 08:11:36 +0000

    Updated Localhost.

--- delaphone/toolbox/localhost.py
+++ delaphone/toolbox/localhost.py
@@ -1,11 +1,24 @@
+import time
 import subprocess
+from subprocess import Popen, PIPE
+import shlex
+from delaphone.toolbox.host import Host
 
-class Localhost:
+class Localhost(Host):
     def __init__(self, **kwargs):
-        self.host = 'localhost'
+        super().__init__(**kwargs)
 
     def run(self, cmd):
-        pass
+        response = subprocess.run(cmd, stdout=PIPE, stderr=PIPE)
+        return response.stdout()
 
     def sudo(self, cmd, **kwargs):
-        pass
+        if 'user' in kwargs:
+            cmd = 'sudo -Su {} {}'.format(kwargs['user'], cmd)
+        else:
+            cmd = 'sudo -S {}'.format(cmd)
+        args = shlex.split(cmd)
+        with Popen(args, stdout=PIPE, stderr=PIPE, stdin=PIPE) as proc:
+            passwd = "{}\n".format(self.password).encode()
+            stdout, stderr = proc.communicate(passwd)
+        return stdout
