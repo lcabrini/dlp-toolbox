abdf40abdccf301a7fb7eca6bc5ab958a2577a18
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-17 23:36:36 +0000

    Added mkdir method.

--- delaphone/toolbox/system/linux.py
+++ delaphone/toolbox/system/linux.py
@@ -1,3 +1,5 @@
+class CommandFailed(Exception): pass
+
 class Linux:
     def __init__(self, host):
         self.host = host
@@ -13,6 +15,15 @@ class Linux:
         if ret == 0:
             return out
 
+    def mkdir(self, path, **kwargs):
+        """
+        Creates the directory *path* or raises *CommandFailed* if it
+        was not possible to create the directory.
+        """
+        cmd = "/usr/bin/mkdir {}".format(path)
+        self._call(cmd, **kwargs)
+        return 0
+
     def tail_messages(self):
         cmd = 'tail -n 10 /var/log/messages'
         ret, out, err = self.host.sudo(cmd)
@@ -21,6 +32,23 @@ class Linux:
         else:
             # TODO: raise an error?
             return err
+
+    def _call(self, cmd, **kwargs):
+        """
+        Helper method for calling Linux commands. If the keyword
+        argument sudo is True, then then the command will be called
+        as the user specified in the 'user' keyword argument (default
+        root), otherwise it will be called as the current user.
+        """
+        if 'sudo' in kwargs and kwargs['sudo'] is True:
+            ret, out, err = self.host.sudo(cmd)
+        else:
+            ret, out, err = self.host.run(cmd)
+
+        if ret == 0:
+            return ret, out
+        else:
+            raise CommandFailed(err)
 
     def deploy(self):
         pass
