91cad707ff15dc59592ef49e565d318e00de157a
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-17 10:30:17 +0000

    Removed unecessary check.

--- delaphone/toolbox/localhost.py
+++ delaphone/toolbox/localhost.py
@@ -2,7 +2,7 @@ import time
 import subprocess
 from subprocess import Popen, PIPE
 import shlex
-from delaphone.toolbox.host import Host
+from delaphone.toolbox.host import Host, NoSuchCommand, MissingPassword
 
 class Localhost(Host):
     def __init__(self, **kwargs):
@@ -18,8 +18,8 @@ class Localhost(Host):
             raise NoSuchCommand(args[0])
 
     def sudo(self, cmd, **kwargs):
-        if not 'password' in kwargs:
-            raise MissingPassword()
+        #if not 'password' in kwargs:
+        #    raise MissingPassword()
 
         if 'user' in kwargs:
             cmd = 'sudo -Su {} {}'.format(kwargs['user'], cmd)
