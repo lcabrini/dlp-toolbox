48ae8cc6795bfc1cd735ab8255b549b1ce9b3c9a
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-17 00:25:32 +0000

    Fixed up Tool class.

--- delaphone/toolbox/tool.py
+++ delaphone/toolbox/tool.py
@@ -1,9 +1,8 @@
-from delaphone.toolbox.localhost import Localhost
-from delaphone.toolbox.remote import RemoteHost
-
 class Tool:
     def __init__(self, **kwargs):
         if 'host' in kwargs:
-            self.handler = RemoteHost(kwargs)
+            from delaphone.toolbox.remote import RemoteHost
+            self.handler = RemoteHost(**kwargs)
         else:
-            self.handler = Localhost(kwargs)
+            from delaphone.toolbox.localhost import Localhost
+            self.handler = Localhost(**kwargs)
