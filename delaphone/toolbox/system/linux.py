4946caee522f22f826ed395cec5df5ab44f2a28d
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-17 16:51:54 +0000

    Added some linux commands.

--- delaphone/toolbox/system/linux.py
+++ delaphone/toolbox/system/linux.py
@@ -6,3 +6,23 @@ class Linux:
         cmd = "test -f {}".format(path)
         ret, out, err = self.host.run(cmd)
         return ret == 0
+
+    def ls(self, path):
+        cmd = "/bin/ls {}".format(path)
+        ret, out, err = self.host.run(cmd)
+        #print("In ls, OUT: {}".format(out))
+        print("RET: {}".format(ret))
+        if ret == 0:
+            return out
+
+    def tail_messages(self):
+        cmd = 'tail -n 10 /var/log/messages'
+        ret, out, err = self.host.sudo(cmd)
+        if ret == 0:
+            return out
+        else:
+            # TODO: raise an error?
+            return err
+
+    def deploy(self):
+        pass
