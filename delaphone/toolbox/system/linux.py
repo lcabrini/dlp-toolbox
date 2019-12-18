e494f75b38153e300152c3fad7c6c7409e577cfc
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-18 07:13:01 +0000

    License header.

--- delaphone/toolbox/system/linux.py
+++ delaphone/toolbox/system/linux.py
@@ -1,3 +1,9 @@
+# Copyright 2019 Lorenzo Cabrini
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
 class CommandFailed(Exception): pass
 
 class Linux:
